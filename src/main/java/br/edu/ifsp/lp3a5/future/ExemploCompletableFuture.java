244b68d7b53675d7048d28a735c8227392a4b99a
Diego <diego2caldas@yahoo.com.br>
2022-09-12 19:10:31 -0300

    Update ExemploCompletableFuture.java

--- src/main/java/br/edu/ifsp/lp3a5/future/ExemploCompletableFuture.java
+++ src/main/java/br/edu/ifsp/lp3a5/future/ExemploCompletableFuture.java
@@ -1,5 +1,7 @@
 package br.edu.ifsp.lp3a5.future;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Future;
 import java.util.concurrent.ThreadLocalRandom;
@@ -7,15 +9,16 @@ import java.util.concurrent.TimeUnit;
 
 public class ExemploCompletableFuture {
 	
-	public double getValorEmAlgumaAPIQualquer() throws InterruptedException {
-		return simulaPegarValorRemoto();
+	public double getValorSync() throws InterruptedException {
+		return simularPegarValorRemoto();
 	}
-	
+
     public Future<Double> getValorAsync() {
         CompletableFuture<Double> valorFuturo = new CompletableFuture<>();
+        
         new Thread(() -> {
             try {
-                valorFuturo.complete(simulaPegarValorRemoto());
+                valorFuturo.complete(simularPegarValorRemoto());
             } catch (Exception e) {
             	valorFuturo.completeExceptionally(e);
             }
@@ -23,9 +26,44 @@ public class ExemploCompletableFuture {
         return valorFuturo;
     }
     
-    private double simulaPegarValorRemoto() throws InterruptedException {
-        TimeUnit.SECONDS.sleep(2);;
+    private double simularPegarValorRemoto() throws InterruptedException {
+        TimeUnit.SECONDS.sleep(2);
         return ThreadLocalRandom.current().nextDouble() * 100;
+    }
+    
+    
+    public static void main( String args[]) throws Exception {
+    	exemploAssincrono();
+    	exemploSincrono();
+    }
+
+        private static void exemploAssincrono() throws Exception {    	
+    	long ini = System.currentTimeMillis();    
+    	List<Future<Double>> listaFuture = new ArrayList<>();
+    	
+    	for (int i = 0; i < 365; i++) {
+        	ExemploCompletableFuture exemplo1 = new ExemploCompletableFuture();
+        	listaFuture.add(exemplo1.getValorAsync());
+    	}
+    	
+    	for(Future<Double> future : listaFuture ) {
+    		System.out.println( future.get() );	
+    	}
+    	
+    	System.out.println( "Tempo de execução async: " + ( System.currentTimeMillis() - ini ) );
+    }
+    private static void exemploSincrono() throws Exception {
+    	ExemploCompletableFuture exemplo1 = new ExemploCompletableFuture();
+    	ExemploCompletableFuture exemplo2 = new ExemploCompletableFuture();
+    	ExemploCompletableFuture exemplo3 = new ExemploCompletableFuture();
+    	
+    	long ini = System.currentTimeMillis();
+    	System.out.println( exemplo1.getValorSync() );
+    	System.out.println( exemplo2.getValorSync() );
+    	System.out.println( exemplo3.getValorSync() );
+    	
+    	System.out.println( "Tempo de execução sync : " + ( System.currentTimeMillis() - ini ) );
+    	
     }
 
 }
